580f3e1e1710b1f594797614f387b64a699487a0
Dhruv Maheshwari <dhwari@gmail.com>
2015-01-17 12:34:47 -0500

    some cloudcode stuff

--- cloudCode/cloud/main.js
+++ cloudCode/cloud/main.js
@@ -4,3 +4,6 @@
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
+
+
+var client = require('twilio')('AC8815762514fc3fd2ba16c0fc8e2c5d81', '3132ecbec50c5939b917389924c2e1bd');
