5a1dfc760a5c00fc9dc58b7a388daf271b72db35
Sacha Best <sachabest@outlook.com>
2015-01-17 15:44:15 -0500

    People picker working (only phone numbers)

---
+++ MemoBox/MemoTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MemoTableViewController.h
+//  MemoBox
+//
+//  Created by Sacha Best on 1/17/15.
+//  Copyright (c) 2015 MemoBox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MemoTableViewController : UITableViewController
+
+@end
